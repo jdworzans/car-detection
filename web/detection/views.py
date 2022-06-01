7402a0cef516ed2808a562c7499e92eaa79395cc
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-06-01 22:01:29 +0200

    Add view for image processing

--- web/detection/views.py
+++ web/detection/views.py
@@ -1,4 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
 
 def index(request):
     return render(request, "detection/index.html")
+
+def detect_cars(request):
+    return HttpResponse("empty")
