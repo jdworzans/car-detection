b8eb5906e494fdf057f7d25a3ed879428f9dba43
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-06-01 18:08:35 +0200

    Change and remove secrets and remove debug by default

--- web/car_detection/settings.py
+++ web/car_detection/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4862u-^fs&6-_!q0*#7m09!#c5qz)eykz-7ipbi4@mcc@y&6(q'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = ["car-detection-atl.herokuapp.com"]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'car-detection-atl.herokuapp.com').split(",")
 
 
 # Application definition
